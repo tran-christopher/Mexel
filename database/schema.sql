c0af0b478212ab93acfaf00b44cc7dfbe98766a9
Christopher Tran <tran.christopher09@gmail.com>
2024-01-19 19:40:28 +0000

    Initial commit

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,35 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "Users" (
+  "username" text,
+  "password" text,
+  "userId" serial PRIMARY KEY
+);
+
+CREATE TABLE "Songs" (
+  "songId" serial PRIMARY KEY,
+  "userId" integer,
+  "title" text,
+  "duration" integer
+);
+
+CREATE TABLE "Playlists" (
+  "playlistId" serial PRIMARY KEY,
+  "title" text,
+  "userId" integer
+);
+
+CREATE TABLE "PlaylistSongs" (
+  "songId" integer,
+  "playlistId" integer
+);
+
+ALTER TABLE "Songs" ADD FOREIGN KEY ("userId") REFERENCES "Users" ("userId");
+
+ALTER TABLE "Playlists" ADD FOREIGN KEY ("userId") REFERENCES "Users" ("userId");
+
+ALTER TABLE "PlaylistSongs" ADD FOREIGN KEY ("songId") REFERENCES "Songs" ("songId");
+
+ALTER TABLE "PlaylistSongs" ADD FOREIGN KEY ("playlistId") REFERENCES "Playlists" ("playlistId");
