448a0483227f893778f6f1780223e1d8f325393c
Christopher Tran <tran.christopher09@gmail.com>
2024-01-22 19:43:29 +0000

    Added post method for sign-up

--- client/src/Components/SignInPage.tsx
+++ client/src/Components/SignInPage.tsx
@@ -1,16 +1,38 @@
+import { FormEvent, useState } from 'react';
+
 export function SignInPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log(`Username: ${username} Password: ${password}`);
+  }
+
   return (
     <div>
-      <form>
-        <div>
-          <h1>Mexel</h1>
-          <button type="button">Sign up</button>
-          <button type="button">Log in</button>
-        </div>
+      <div>
+        <h1>Mexel</h1>
+        <button type="button">Sign up</button>
+        <button type="button">Log in</button>
+      </div>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="username">Username</label>
-        <input type="text" name="username" />
+        <input
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+          type="text"
+          name="username"
+        />
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" />
+        <input
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          type="password"
+          name="password"
+        />
         <button type="submit">Sign in</button>
         <p>Don't have an account? Sign up here</p>
       </form>
--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,19 @@
 /* eslint-disable @typescript-eslint/no-unused-vars -- Remove when used */
 import 'dotenv/config';
 import express from 'express';
+import argon2 from 'argon2';
 import pg from 'pg';
 import {
   ClientError,
   defaultMiddleware,
   errorMiddleware,
 } from './lib/index.js';
+
+type User = {
+  userId: number;
+  username: string;
+  hashedPassword: string;
+};
 
 const connectionString =
   process.env.DATABASE_URL ||
@@ -29,8 +36,28 @@ app.use(express.static(reactStaticDir));
 app.use(express.static(uploadsStaticDir));
 app.use(express.json());
 
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello, World!' });
+const hashKey = process.env.TOKEN_SECRET;
+if (!hashKey) throw new Error('TOKEN_SECRET not found in .env');
+
+app.post('/api/sign-up', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      throw new ClientError(400, 'username and password are required fields');
+    }
+    const hashedPassword = await argon2.hash(password);
+    const sql = `
+        insert into "Users" ("username", "hashedPassword")
+        values ($1, $2)
+        returning *
+        `;
+    const params = [username, hashedPassword];
+    const result = await db.query<User>(sql, params);
+    const [user] = result.rows;
+    res.status(201).json(user);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 /*
